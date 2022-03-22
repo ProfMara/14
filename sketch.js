423f00d6c05ad4b1521ad9a0192b2ac0e43f8a8b
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-03-22 17:41:02 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
-var bola;
+var bola = {
+    x: 20,
+    y: 50,
+    raio: 20,
+    cor: "green"
+};
 
 function setup() {
     createCanvas(800, 400);
-
+    console.log(bola);
 
 }
 
 function draw() {
     background("cyan");
     drawSprites();
+    fill(bola.cor)
+    ellipse(bola.x, bola.y, bola.raio);
 
 }
